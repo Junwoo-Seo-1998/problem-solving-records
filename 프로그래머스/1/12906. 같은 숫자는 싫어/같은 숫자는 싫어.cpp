b60c241c29a1f0b314dcb5198b18bb103edb0e54
Junwoo-Seo <junwoo.seo.1998@gmail.com>
2025-05-14 21:17:36 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 111.99 ms, Memory: 105 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr) 
+{
+    vector<int> answer;
+
+    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    cout << "Hello Cpp" << endl;
+    for(int i=0; i<arr.size(); ++i)
+    {
+        if(answer.size() == 0 || *(answer.end()-1) != arr[i])
+            answer.push_back(arr[i]);
+    }
+    return answer;
+}
