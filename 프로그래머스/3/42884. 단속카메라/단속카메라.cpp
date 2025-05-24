57bba1d088e7dde52ec1ab7d27c318b3b537efee
Junwoo-Seo <junwoo.seo.1998@gmail.com>
2025-05-24 23:34:36 +0900

    [level 3] Title: 단속카메라, Time: 0.59 ms, Memory: 4.04 MB -BaekjoonHub

---
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int solution(vector<vector<int>> routes) {
+    sort(routes.begin(), routes.end(), [](const vector<int>& a, const vector<int>& b){
+        return a[1] < b[1];
+    });
+    int camera=-30001;
+    int answer = 0;
+    
+     for (const auto& route : routes) 
+     {
+        if (route[0] > camera) 
+        {
+            camera = route[1];
+            answer++;
+        }
+    }
+    
+    return answer;
+}
