63d9f05f4cdd4683b7c3686b3b70c12284a9e767
Junwoo-Seo <junwoo.seo.1998@gmail.com>
2025-08-26 19:28:15 +0900

    [Platinum IV] Title: 열혈강호, Time: 356 ms, Memory: 6004 KB -BaekjoonHub

---
+++ 백준/Platinum/11375. 열혈강호/열혈강호.cc
@@ -0,0 +1,64 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+vector<vector<int>> people;
+int N, M;
+
+vector<int> job;
+vector<bool> asked;
+
+bool match(int current)
+{
+    for(int j:people[current])
+    {
+        if(asked[j]) continue;
+        asked[j]=true;
+        if(job[j]==-1 || match(job[j]))
+        {
+            job[j]=current;
+            return true;
+        }
+    }
+
+    return false;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    //freopen("input.txt", "r", stdin);
+    cin>>N>>M;
+
+    job.assign(M+1, -1);
+    people.assign(N+1, {});
+    for(int i=1; i<=N; ++i)
+    {
+        int J;
+
+        cin>>J;
+        people[i].reserve(J);
+        for(int j=0; j<J; ++j)
+        {
+            int job;
+            cin>>job;
+            people[i].push_back(job);
+        }
+    }
+
+    int cnt=0;
+
+    for(int i=1; i<=N; ++i)
+    {
+        asked.assign(M+1, false);
+        if(match(i))
+        {
+            cnt++;
+        }
+    }
+
+    cout<<cnt;
+
+    return 0;
+}
