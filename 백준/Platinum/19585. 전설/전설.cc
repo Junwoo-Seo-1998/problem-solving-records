8491f17f59dd8d3b7f0460e7d9e1f4938159ad38
Junwoo-Seo <junwoo.seo.1998@gmail.com>
2025-08-28 23:38:15 +0900

    [Platinum III] Title: 전설, Time: 2244 ms, Memory: 829568 KB -BaekjoonHub

---
+++ 백준/Platinum/19585. 전설/전설.cc
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_set>
+#include <unordered_map>
+
+using namespace std;
+
+struct ColorTrie {
+    struct Node {
+        unordered_map<char, int> childIndex;
+        bool isColorEnd; 
+
+        Node() : isColorEnd(false) {}
+    };
+
+    vector<Node> nodes;
+
+    ColorTrie() {
+        nodes.emplace_back(); 
+    }
+
+    void insert(const string& str) {
+        int cur = 0;
+        for (char c : str) {
+            if (nodes[cur].childIndex.count(c) == 0) {
+                nodes[cur].childIndex[c] = nodes.size();
+                nodes.emplace_back();
+            }
+            cur = nodes[cur].childIndex[c];
+        }
+        nodes[cur].isColorEnd = true; 
+    }
+};
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    int C, N;
+    cin >> C >> N;
+
+    ColorTrie colorTrie;
+    for (int i = 0; i < C; ++i) {
+        string color;
+        cin >> color;
+        colorTrie.insert(color);
+    }
+
+    unordered_set<string> nicknameSet;
+    for (int i = 0; i < N; ++i) {
+        string nickname;
+        cin >> nickname;
+        nicknameSet.insert(nickname);
+    }
+  
+    int Q;
+    cin >> Q;
+    while (Q--) {
+        string word;
+        cin >> word;
+
+        bool found = false;
+        int currentNodeIndex = 0; 
+
+        for (int i = 0; i < word.length(); ++i) {
+            char c = word[i];
+
+            if (colorTrie.nodes[currentNodeIndex].childIndex.count(c) == 0) {
+                break;
+            }
+            currentNodeIndex = colorTrie.nodes[currentNodeIndex].childIndex.at(c);
+
+            if (colorTrie.nodes[currentNodeIndex].isColorEnd) {
+
+                string suffix = word.substr(i + 1);
+
+                if (nicknameSet.count(suffix)) {
+                    found = true;
+                    break; 
+                }
+            }
+        }
+
+        if (found) {
+            cout << "Yes\n";
+        } else {
+            cout << "No\n";
+        }
+    }
+
+    return 0;
+}
