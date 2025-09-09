2cb9e51eb94883c8dcb3b3eeeb98cc6ef4ba6831
Junwoo-Seo <junwoo.seo.1998@gmail.com>
2025-09-09 21:46:25 +0900

    Title: 친구의 키, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250909/친구의 키/height-of-friends.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int n, m;
+int a[100000], b[100000];
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    cin >> n >> m;
+    for (int i = 0; i < m; i++) {
+        cin >> a[i] >> b[i];
+    }
+
+
+    
+
+    // Please write your code here.
+
+    return 0;
+}
