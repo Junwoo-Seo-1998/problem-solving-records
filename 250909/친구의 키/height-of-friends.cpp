7ad41859ea0fc5f1b4e8cd6c87e833f19c85cca3
Junwoo-Seo <junwoo.seo.1998@gmail.com>
2025-09-09 21:52:38 +0900

    Title: 친구의 키, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

--- 250909/친구의 키/height-of-friends.cpp
+++ 250909/친구의 키/height-of-friends.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
-
+#include <vector>
+#include <stack>
 using namespace std;
+//dfs로 위상정렬 하는거 연습해보는 용
+
 
 int n, m;
 int a[100000], b[100000];
+
+int visited[100'001];
+vector<int> edge[100'001];
+
+stack<int> sorted;
+
+void dfs()
+{
+
+    sorted.push();
+}
+
 
 int main() {
     ios_base::sync_with_stdio(false);
@@ -13,10 +28,12 @@ int main() {
         cin >> a[i] >> b[i];
     }
 
+    for(int i=0; i<m; ++i)
+    {
+        edge[a[i]].push_back(b[i]);
+    }
 
-    
 
-    // Please write your code here.
-
+    while(!sorted.empty())
     return 0;
 }
