2118e2fb888c863fb43e316c93d86f5db8d59a17
Junwoo-Seo <junwoo.seo.1998@gmail.com>
2025-10-27 23:33:38 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+	int t;
+	cin >> t;
+	
+	for(int i = 1; i <= t; i++) {
+		for(int k = 0; k < i; k++) {
+			cout << "*";
+		}
+		cout << '\n';
+	}
+	
+	return 0;
+}
