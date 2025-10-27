14eb92b826d1f8333994a4ed6a14dc9ca86e09c1
Junwoo-Seo <junwoo.seo.1998@gmail.com>
2025-10-27 23:26:33 +0900

    [Bronze V] Title: 등록, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    std::cout.tie(NULL);
+
+    std::cout << 60 << '\n';
+    std::cout << "junwoo_seo" << '\n';
+
+    return 0;
+}
